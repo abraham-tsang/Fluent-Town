be09b6e188aa815cd2c90b581aab7670f74762ba
Abraham Tsang <abraham.tsang@gmail.com>
2021-01-11 09:57:01 -0500

    first commit

--- App.js
+++ App.js
@@ -1,16 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, TextInput, FlatList, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const firstOnPress = () => {
+    console.log("test")
+};
+
+const firstOnSubmitEditing = () => {
+    console.log("test2")
+};
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+export default class App extends React.Component{
+
+
+    render(){
+
+    const renderItem = ({ item }) => (
+        <Item title={item.title} />
+    );
+        return(
+	    <View style={styles.container}>
+		<Text>Open up App.js to start working on your app!</Text>
+		<Button title="Press me" onPress={firstOnPress} />
+		<TextInput style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1 }} onSubmitEditing={firstOnSubmitEditing} />
+
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+      />
+
+	    </View>
+	);
+    }
 }
 
+/*
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -19,3 +64,65 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+import React from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const App = () => {
+  const renderItem = ({ item }) => (
+    <Item title={item.title} />
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+      />
+    </SafeAreaView>
+  );
+}
+*/
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+  subtitle: {
+    fontSize: 16,
+  },
+});
+
+//export default App;
